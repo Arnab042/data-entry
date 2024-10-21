efa8db9f1ce05d05c6afe0740842518761f2a0a9
Arnab042 <arnabmandal056@gmail.com>
2024-10-21 21:02:20 +0530

    index.js

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
-let myname;
-let result;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Data</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+      
+    <div class="container">
+        <label for="">What is your name?</label>
+    
+        <input id="user" >
+        <br>
+        <button id="B1"> submit</button>
+        <button onClick="history.go(0);">Reload</button>
 
-
-document.getElementById('B1').onclick = function(){
-    myname = document.getElementById('user').value ;
-    myname = String(myname);    
-    console.log(myname);
-    if(myname){
-        document.getElementById('myh1').textContent = myname ;
-        if(myname == 'arnab' || myname =='ARNAB' || myname == 'Arnab' ){
-            document.getElementById('myh1').textContent = "oh Hi Arnab!!"
-            document.getElementById('yourdata').textContent = "NAME - Arnab Mandal  , AGE : 23 Years , Address : Sonarpur"
-            document.getElementById('image1').src = 'me.png';
-        }
-        else if(myname == 'debarghya' || myname == 'Debarghya' || myname== 'DEBARGHYA' ){
-            document.getElementById('myh1').textContent = "Your are Debarghya Welcome!!"
-            document.getElementById('yourdata').textContent = "NAME - Debarghya Mukherjee  , AGE : 23 Years , Address : Sahid khudiram"
-            document.getElementById('image2').src = 'deb.png';
-        }
-        else{
-            document.getElementById('myh1').textContent = "you are not anyone!"
-        }
-    }
-    else if (!myname){
-        document.getElementById('myh1').textContent = 'please enter your name first';
-    }
+        
+    </div>
+    <h1 id= "myh1"> hey you </h1>
+    <sec id="yourdata">Your data will come here <br>
+    
+    </sec>
+    <div id="img" style="overflow: hidden;">
+        <img src=""  style="width: auto;  height: 20vh; margin-left: 5px; margin-top: 8px; display: flex;"   id="image1" >
+        <img src=""    style="width: auto;    height: 20vh; margin-left: 5px; margin-top: -18vh; display: flex ;"   id="image2" >
+        <img src=""    style="width: auto;    height: 20vh; margin-left: 5px; margin-top: -18vh; display: flex ;"   id="image3" >
+        <img src=""    style="width: auto;    height: 20vh; margin-left: 5px; margin-top: -18vh; display: flex ;"   id="image4" >
+        <img src=""    style="width: auto;    height: 20vh; margin-left: 5px; margin-top: -18vh; display: flex ;"   id="image5" >
+    </div>
     
 
-}
-
+    
+    
+<script src="index.js"></script>   
+</body>
+</html>
